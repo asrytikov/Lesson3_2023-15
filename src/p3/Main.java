d7ab1417e25e8eae7a50ba0cb86a270fc887d4fc
asrytikov <asrytikov@gmail.com>
2023-09-22 10:30:51 +0400

    Lesson3

--- src/p3/Main.java
+++ src/p3/Main.java
@@ -1,2 +1,22 @@
-package p3;public class Main {
+package p3;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input name: ");
+        String name = in.nextLine();
+        System.out.print("Input age: ");
+        int age = in.nextInt();
+        System.out.print("Input height: ");
+        float height = in.nextFloat();
+        System.out.printf("Name: %s Age: %d Height: %.2f \n", name, age, height);
+        in.close();
+
+
+    }
+
 }
--- src/p4/Main.java
+++ src/p4/Main.java
@@ -1,2 +1,65 @@
-package p4;public class Main {
+package p4;
+
+import java.math.BigDecimal;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 15;
+        double z = 15;
+
+        int a = x+y;
+        int b = 10+x;
+        int c = x-y;
+        int d = 10-x;
+        int e = x*y;
+        int f = x/y;
+        double f1 = (double)x / y;
+        System.out.println(f1);
+
+        int g = y%x;
+        System.out.println(g); // (15 - 10*1 = 5)
+
+        int h = ++y; //y = y + 1 --y;
+        System.out.println(h);
+        System.out.println(y);
+
+        int k = x++; // x = x+1 y--;
+        System.out.println(k);
+        System.out.println(x);
+
+        // 1 приоритет-  x++, y--
+        // 2 приоритет- ++x, --y
+        // 3 приоритет - *x, /x, %x
+        // 4 приоритет - +, -
+
+       // int m = x+5 * ++y;
+        //System.out.println(m);
+        int m1 = (x+5) * ++y;
+        System.out.println(m1);
+
+        int n = 10/5*2;
+        System.out.println(n);
+
+        double l = 2.0 - 1.1;
+        System.out.println(l);
+
+        byte b1 = 7; // 0000 0111
+        short s1 = 7; // 0000 0000 0000 0111
+
+        //1 - 0000 0001
+        // -1 - 1111 1111
+        // 1111 0011 -13
+
+
+
+
+
+
+
+
+    }
+
 }
